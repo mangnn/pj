70a39be38bd63c42c1de0402f1c1a5df37ec348a
Mang-na-nyong <scy7920@gmail.com>
2025-01-14 08:33:47 +0900

    f

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function pwc(){
     var pw=prompt("비밀번호를 입력하세요");
     if (pw == 789456){
-        location.href="https://mangnn.github.io/pj-vuswl/"
+        location.href="./편지/index2.html"
     }
     else{
         alert("비밀번호가 틀렸습니다")
